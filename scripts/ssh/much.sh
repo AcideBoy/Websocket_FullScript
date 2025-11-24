2d4a8190cb59850b7f92c4a97f3acffe0578bce8
AcideBoy <mohamedstudy00@gmail.com>
2025-11-24 01:39:52 +0100

    Update much.sh

--- scripts/ssh/much.sh
+++ scripts/ssh/much.sh
@@ -1,5 +1,5 @@
 gum format --theme dracula --type markdown <<EOF
 This is how much connected to your Server
 EOF
-echo $( $(ss -tnp | grep ':7080' | grep -c 'nginx'))
+echo $(ss -tnp | grep ':7080' | grep -c 'nginx')
 gum confirm "Return to menu?" && asx
